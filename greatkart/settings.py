c62a82f6166a1497d8de1952c4d4052f7f3e1e29
Anshuman Singh <anshumansingh9807@gmail.com>
2025-07-19 14:32:54 +0530

    cart counter implement

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processors.menu_links',  # Custom context processor for categories
+                'carts.context_processors.counter',
             ],
         },
     },
