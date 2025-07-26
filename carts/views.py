75a4e4e43cda3cedbe1d6f0cd0d5a3f9f552df64
Anshuman Singh <anshumansingh9807@gmail.com>
2025-07-26 21:11:57 +0530

    Add product variation model

--- carts/views.py
+++ carts/views.py
@@ -15,6 +15,9 @@ def _cart_id(request):
     return cart
 
 def add_cart(request, product_id):
+    color = request.GEt['color']
+    size = request.GET['size']
+    
     product = Product.objects.get(id = product_id)
     try:
         cart = Cart.objects.get(cart_id = _cart_id(request))
